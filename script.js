5b09fb79891329f567c2d383e325d4e39a0e2683
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-07 16:58:19 -0300

    usando variaveis + setAttributes para trocar imagens quuando um um determinado botão é clicado

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const  html = document.querySelector('html')
-const focoBt = document.querySelector(' .app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo')
+const  html = document.querySelector('html');
+const focoBt = document.querySelector(' .app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.another-image') ;
 
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-})
+
+  focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
+}) 
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    html.setAttribute('data-contexto', 'descanso-curto');
+
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 })
 
 longoBt.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-longo')
-})
+    html.setAttribute('data-contexto', 'descanso-longo');
+
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
+}) 
