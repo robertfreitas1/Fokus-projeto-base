e96dfcca56225c0162caf06ff1bc8813bccffb0f
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-10 11:40:20 -0300

    alterando texto da página

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector(' .app__card-button--foco');//pega a class
 const curtoBt = document.querySelector('.app__card-button--curto');//pega a classe app__card-button--curto
 const longoBt = document.querySelector('.app__card-button--longo'); //pega a classe app__card-button--longo
 const banner = document.querySelector('.another-image') ; //pega a classe another-image
+const titulo  = document.querySelector('.app__title'); //pega o titulo da página que tem a classe app__title
 
 
   focoBt.addEventListener('click', () => {
@@ -21,4 +22,24 @@ longoBt.addEventListener('click', () =>{
 function alterarContexto (contexto){
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `  Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = ` Que tal da uma respirada?,<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+
+
+             break;
+         case "descanso-longo":
+                titulo.innerHTML = `Hora de voltar a superfície,<br>
+                        <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+         
+               default:
+               break;
+    }
 }
